21efaae6573cb00b4764436830bd06f20e6afe1d
odriann <odriann23@gmail.com>
2016-11-02 15:30:27 +0000

    first saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,22 @@
+var greeting = 'Hello, everybody!'
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "Hello," + specialGuest + "!";
+
+var topic = "space";
+
+var interpolatedString = ('let\'s talk bout ${topic}.')
+
+var conversation = `Let\'s talk about ${topic}.`;
+
+var writeGreetingToDocument = `something to do with document...`;
+
+// Type your answer into the console without the quote marks and see if it works.
+
+var customCelebration = 'Kaboomie! Hachewie, yeaaaaaaaah. Shwing.';
+
+var consoleCelebration = 'something to do with console...'
+var 'something to do with console...' = 'console.log(customCelebration);'
+
+
